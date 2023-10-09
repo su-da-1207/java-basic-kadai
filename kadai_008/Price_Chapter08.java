690818171c9a2098d9749bb72616ac5c7351ef8c
su-da-1207 <135832307+su-da-1207@users.noreply.github.com>
2023-10-09 19:40:03 +0900

    Price_Chapter08.javaを追加

--- kadai_008/Price_Chapter08.java
+++ kadai_008/Price_Chapter08.java
@@ -1,1 +1,24 @@
+package Price_Chapter08.java;
 
+public class Price_Chapter08 {
+	public static void main(String args[]) {
+		
+		int userAge = 30;
+		int serviceCost = 3000;
+		
+		serviceCost = switch(userAge) {
+		case 10 -> 1000;
+		case 20 -> 2000;
+		case 30 -> 3000;
+		case 40 -> 3000;
+		case 50 -> 4000;
+		case 60 -> 4000;
+		case 70 -> 4000;
+		case 80 -> 5000;
+		default -> 500;
+		};
+		
+		System.out.println(userAge + "代の料金は" + serviceCost + "円");
+	}
+
+}
