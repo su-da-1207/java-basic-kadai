1b23133296379f1844849c416dc8f91635360b9b
su-da-1207 <135832307+su-da-1207@users.noreply.github.com>
2023-10-15 17:11:39 +0900

    Kato_Chapter17を追加

---
+++ kadai_017/KatoTaro_Chapter17.java
@@ -0,0 +1,15 @@
+package kadai_017;
+
+public class KatoTaro_Chapter17 extends Kato_Chapter17 {
+	
+	//名を表すフィールドの値をセットする
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+	
+	//eachIntroduceの実装
+	public void eachIntroduce() {
+		System.out.println("私はJavaが得意です");
+	}
+	
+}
